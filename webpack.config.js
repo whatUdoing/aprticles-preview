30a0a6b468f8a760ca8b79528f9e769484887a19
JoannFirek <joanna.firek92@gmail.com>
2020-05-09 22:47:22 +0200

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist',
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            }
+module.exports = (env, argv) => {
+    return {
+        entry: './src/index.tsx',
+        output: {
+            filename: 'bundle.js',
+            chunkFilename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        devServer: {
+            contentBase: './dist',
+            port: 3000
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', 'scss']
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.relative(__dirname, './src/index.html')
+            }),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin()
         ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', 'scss']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.relative(__dirname, './src/index.html')
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin()
-    ]
+    };
 };
